7058522b03b43a4a14d72052d3fc55525c7a87aa
Ninjaphil24 <mphilippos@hotmail.com>
2022-02-25 08:42:05 +0200

    Complete Tutorial

---
+++ basics.js
@@ -0,0 +1,10 @@
+// setItem takes two parameters: Key and Value
+localStorage.setItem("name", "Phil")
+// numbers can be integers or floats out of quotes
+        localStorage.setItem("age", "30")
+// removeItem and getItem takes one parameter: Key
+        // localStorage.removeItem("name");
+        // localStorage.getItem("name");
+        console.log(localStorage.getItem("age"))
+// the key method finds keys via index
+        console.log(localStorage.key(0))
